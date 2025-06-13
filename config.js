5370bcdcdd35a1081fc236bd36cdd0fd30416008
kunal-a11y <mindcraftgamer26@gmail.com>
2025-06-13 13:09:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     modmail: {
-        ownerID: '', // ← Insert the Server Owner ID.
-        supportId: '', // ← Insert General Support Category ID.
-        premiumId: '', // ← Insert Premium Support Category ID.
-        whitelist: '', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
-        staff: '', // ← Insert Staff Role to be pinged upon ticket creation.
+        ownerID: '1108027593011236895', // ← Insert the Server Owner ID.
+        supportId: '1382986522487160933', // ← Insert General Support Category ID.
+        premiumId: '1368104914185748623', // ← Insert Premium Support Category ID.
+        whitelist: '1368104914185748623', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
+        staff: '1382986835248021648', // ← Insert Staff Role to be pinged upon ticket creation.
     },
     logs: {
-        logschannel: '' // ← Insert Ticket Logs Channel ID.
+        logschannel: '1382986522487160933' // ← Insert Ticket Logs Channel ID.
     }
 };
